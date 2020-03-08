c29063bb10e1f86af0b624b69cdefd44a698f5c4
PiotrKaszuba <piotrkaszuba1996@gmail.com>
2020-03-08 13:29:51 +0100

    lstm simple

--- lstm.py
+++ lstm.py
@@ -1,7 +1,28 @@
 from keras.layers import *
-from keras.models import Model
+from keras.models import Model, Sequential
+from encodeGenos import testEncode
+from keras.preprocessing.sequence import pad_sequences
 
-inpE = Input((10,5)) #here, you don't define the batch size
-outE = LSTM(units = 20, return_sequences=False)(inpE)
+sequences = testEncode()
 
-encoder = Model(inpE,outE)
+sequence = pad_sequences(sequences, padding='post', dtype='float32')
+max_len = sequence.shape[1]
+features = sequence.shape[2]
+
+inp = Input(shape=(max_len, features))
+encoder = LSTM(8, activation='relu')(inp)
+
+# define reconstruct decoder
+decoder1 = RepeatVector(max_len)(encoder)
+decoder1 = LSTM(8, activation='relu', return_sequences=True)(decoder1)
+decoder1 = TimeDistributed(Dense(features))(decoder1)
+
+model = Model(inputs=inp, outputs=decoder1)
+
+
+model.compile(optimizer='adam', loss='binary_crossentropy')
+model.fit(sequence, sequence,
+                      epochs=400,
+                      verbose=2,
+                      validation_split=0.1,
+                      shuffle=True)
