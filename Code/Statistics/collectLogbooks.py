8869151a1ed386877c4d972ec1c00b55b37c725b
PiotrKaszuba <piotrkaszuba1996@gmail.com>
2021-04-06 19:51:49 +0200

    collectLogbooks.py modify

--- Code/Statistics/collectLogbooks.py
+++ Code/Statistics/collectLogbooks.py
@@ -10,10 +10,18 @@ def mkdir_p(dir):
         os.mkdir(dir)
 
 home = str(Path.home())
+suffixes = ['18', '19', '20']
 datafolder = os.path.join(home, 'dataFolder2')
 for fname in glob.glob(datafolder+'/**/logbook_*', recursive=True):
     print(fname)
     dir, f = os.path.split(fname)
+    match = False
+    for suff in suffixes:
+        if dir.endswith(suff):
+            match = True
+            break
+    if not match:
+        continue
     dir, name = os.path.split(dir)
     target_f = os.path.join(datafolder, 'logbooks', name)
     mkdir_p(target_f)
